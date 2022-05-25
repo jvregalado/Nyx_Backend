ac9b167bbadb3fc3c50b7b234e50c1f81b741240
jvregalado <jvregalado@codedisruptors.com>
2022-05-25 14:31:01 +0800

    error null localstring

--- api/tms-reporthub.js
+++ api/tms-reporthub.js
@@ -90,15 +90,17 @@ router.post('/sp_DFDailyMonitoring_cdi', async(req,res) => {
 			const RDDdurationHR = Number((RDD-ActualRDDnum) / (1000 * 60 * 60)).toFixed(2); 
 			const CompliancetoRDD = (((24+Number(RDDdurationHR))/24)*100).toFixed(2);
 
-			const ETDOriginnum = new Date(Date.parse(KronosTripIDline[line]?.ETDoriginDate||null+' '+KronosTripIDline[line]?.ETDoriginTime.toLocaleTimeString()))
+			const ETDOriginTime = new Date(KronosTripIDline[line]?.ETDoriginTime||null);
+			const ETDOriginnum = new Date(Date.parse(KronosTripIDline[line]?.ETDoriginDate||null+' '+ETDOriginTime.toLocaleTimeString()))
 			const ETDOrigin = await helper.formatDateAndTime({toFormat:ETDOriginnum})
 
 			const ATAOriginnum = new Date(KronosTripIDline[line]?.ATDorigin);
 			const ATAOrigin = await helper.formatDateAndTime({toFormat:ATAOriginnum})
 
 			const DeparturePerformance = Number((ETDOriginnum-ATAOriginnum) / (1000 * 60 * 60 * 24)).toFixed(2);
-			 
-			const ETDDestinationnum = new Date(Date.parse(KronosTripIDline[line]?.ETDdestDate||null+' '+KronosTripIDline[line]?.ETDdestTime.toLocaleTimeString()))
+			
+			const ETDDestinationTime = new Date(KronosTripIDline[line]?.ETDdestTime||null)
+			const ETDDestinationnum = new Date(Date.parse(KronosTripIDline[line]?.ETDdestDate||null+' '+ETDDestinationTime.toLocaleTimeString()))
 			const ETDDestination = await helper.formatDateAndTime({toFormat:ETDDestinationnum})
 
 			const ATADestinationnum = new Date(KronosTripIDline[line]?.ATDdest);
@@ -125,35 +127,35 @@ router.post('/sp_DFDailyMonitoring_cdi', async(req,res) => {
 				"Trucker Assignment":KronosTripIDpick[pick]?.TruckAssignment||"",											//11
 				"Plate No. Pick":KronosTripIDpick[pick]?.PlateNo||"",														//12
 				"ATA @ Pickup Location": ATA,																				//13
-				"Call Time duration":ATA?`${CallTimeDuration} Hour(s)`:null,																//14
+				"Call Time duration":ATA?`${CallTimeDuration} Hour(s)`:null,												//14
 				"Compliance to Call Time":ATA?`${CompliancetoCallTime} %`:null,												//15
 				"LCT":LCTStart,																								//16
 				"ATA @ Pier for In-Yard":ATApick,																			//17
-				"LCT Duration":ATApick?`${LCTDuration} Hour(s)`:null,																					//18
-				"Compliance to LCT or In-Yard":ATApick?CompliancetoLCT:null,																//19
+				"LCT Duration":(ATApick&&LCTStart)?`${LCTDuration} Hour(s)`:null,														//18
+				"Compliance to LCT or In-Yard":(ATApick&&LCTStart)?CompliancetoLCT:null,												//19
 				"Booking in SL":result[x].BookingSL||"",																	//20
 				"Container no.":KronosTripIDpick[pick]?.ContainerNo||"",													//21
 				"Vessel & Voyage":KronosTripIDline[line]?.Voyage||"",														//22
 				"Carrier":KronosTripIDline[line]?.Carrier||"",																//23
 				"ETD Origin":ETDOrigin,																						//24
 				"ATD Origin":ATAOrigin,																						//25
-				"Departure Reliability Performance":ETDOrigin?`${DeparturePerformance} Day(s)`:null,													//26
+				"Departure Reliability Performance":ETDOrigin?`${DeparturePerformance} Day(s)`:null,						//26
 				"ETA Destination":ETDDestination,																			//27
 				"ATA Destination":ATADestination,																			//28
-				"Arrival Reliability Performance":ETDDestination?`${ArrivalPerformance} Day(s)`:null,														//29
+				"Arrival Reliability Performance":ETDDestination?`${ArrivalPerformance} Day(s)`:null,						//29
 				"Discharge Port":result[x].DischargePort,																	//30
-				"Actual Pullout from Pier":ApulloutPier,	//31
+				"Actual Pullout from Pier":ApulloutPier,																	//31
 				"Trucker":KronosTripIDdel[del]?.trucker||"",																//32
 				"Plate No. Del":KronosTripIDdel[del]?.PlateNo||"",															//33
 				"Actual delivery to consignee":ActualRDD,																	//34
 				"RDD Duration":ActualRDD?`${RDDduration} Day(s)`:"",														//35
 				"Compliance to RDD":ActualRDD?`${CompliancetoRDD}%`:"",														//36
 				//"Dwell Time":"",																							//37
-				"Storage dwell time (2 days free)":`${StorageDwell} Day(s)`,																		//38		
-				"Shipment No.":result[x].ShipmentNo||"",												//39
-				"Received by":result[x].ReceivedBy,														//40
-				"Status":KronosTripIDdel[del]?.Remarks||"",												//4
-				"Remarks":KronosTripIDdel[del]?.Remarks||""												//4
+				"Storage dwell time (2 days free)":(ATADestination&&ApulloutPier)?`${StorageDwell} Day(s)`:null,		//38
+				"Shipment No.":result[x].ShipmentNo||"",																	//39
+				"Received by":result[x].ReceivedBy,																			//40
+				"Status":KronosTripIDdel[del]?.Remarks||"",																	//42
+				"Remarks":KronosTripIDdel[del]?.Remarks||""																	//43
 			})
 		}
 
